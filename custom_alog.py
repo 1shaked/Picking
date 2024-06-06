f40bc2956e1d6626f6b55c520eec9e61da867a97
1shaked <shaked1hen@gmail.com>
2024-06-06 16:18:42 +0300

    Refactor custom_alog.py to improve order processing efficiency

--- custom_alog.py
+++ custom_alog.py
@@ -1,23 +1,74 @@
+'''
 
-
-orders = {
-    # order number : { items: [
-    #    {
-    #item: 'item name', quantity: 1, location , 
-          # },
-    #    ], 
-    #}
+items_location = {
+    item_id: (x, y),
+    ...
 }
 
-max_batch_size = 25
+location_to_items = {
+
+    (x, y): [ item_id, ... ],
+}
+
+order = [item_id, ...]
+
+orders_dict = {
+    order_id_${num}: [
+        item_id,
+    ],
+}
+'''
+import json
+
+OVERLAP = 'overlap'
+DIFFERENCE = 'difference'
+
+# load the data 
+items_location_x: dict[str, list[int]] = {}
+with open('dummyData/items_location_x.json') as f:
+    items_location_x = json.load(f)
+
+location_to_item_x: dict[str, list[str]] = {}
+with open('dummyData/location_to_item_x.json') as f:
+    location_to_item_x = json.load(f)
+
+orders_dict_x: dict[str, list[str]] = {}
+with open('dummyData/orders_x.json') as f:
+    orders_dict_x = json.load(f)
 
 
-start_order = PickRandomOrder(orders) # this also delete the order from the orders list
+print('items_location_x:', items_location_x)
+print('location_to_item_x:', location_to_item_x)
+print('orders_dict_x:', orders_dict_x)
 
-batches = []
-while len(orders) !== 0:
-    locations = getLocations(batches[-1])
-    while batches[-1].size <= max_batch_size:
-        similar_orders = getMostSimilarOrdersFromLocatio(locations, orders)
-        batches[-1].addOrder(similar_orders[0])
-        PickOrder(similar_orders[0])
+# select an order to process
+orders_to_pick: list[str] = ["order_id_15"]
+location_to_pick: list[int] = [9]
+
+
+# step 1: get all the order and sort by the similarity of the location
+order_to_location: dict[str, dict[str , int]] = {}
+# get the overlap and difference of the order with the location
+for order_key in orders_dict_x:
+    order_items = orders_dict_x[order_key]
+
+
+    order_info = {
+        OVERLAP: 0,
+        DIFFERENCE: 0
+    }
+
+    for item in order_items:
+        # for each item we will check if it is already in the location
+        item_loc = items_location_x[item]
+        # is any of the item in the location_to_pick
+        for loc in location_to_pick:
+            if loc in item_loc:
+                order_info[OVERLAP] += 1
+                break
+            order_info[OVERLAP] += 1
+            
+    order_to_location[order_key] = order_info
+
+
+print('order_to_location:', order_to_location)
