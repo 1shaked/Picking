1bf312ecb2dd6565d4352804896170cbcd5cd493
1shaked <shaked1hen@gmail.com>
2024-06-30 15:58:47 +0300

    chore: Update center_locations.json with additional items and fix get_center_locations_for_item function

--- alog_test_1.py
+++ alog_test_1.py
@@ -5,7 +5,6 @@ import json as js
 orders_df = pd.read_csv('sim/orders_ids.csv')
 products_in_order_df = pd.read_csv('sim/products_in_order.csv')
 
-products_locations = pd.read_excel('sim/products.xlsx')
 
 
 # choose orders where site id is 168
@@ -16,34 +15,8 @@ orders_dict_x: dict[str, list[str]] = {}
 for order in orders:
     # get all the products with this id
     temp = products_in_order_df[products_in_order_df['order_id'] == order]
-    items = list(temp['item_id'].values)
-    orders_dict_x[order] = items
-
-
-# Convert the 'Itur' column to numeric, forcing errors to NaN (you can handle them as needed)
-products_locations['IturAsNumber'] = pd.to_numeric(products_locations['Itur'], errors='coerce')
-
-# Now perform the division and rounding
-products_locations['location'] = (products_locations['IturAsNumber'] / 5).round()
-
-locations: dict[str, list[str]] = {}
-locations_unique = products_locations['location'].unique()
-for location in locations_unique:
-    # check if is nan
-    
-    if np.isnan(location) or location == None:
-        continue
-    # get all products in this location
-    temp = products_locations[products_locations['location'] == location]
-    items = list(temp['item_id'].unique())
-    print(items)
-    locations[str(int(location))] = [str(item) for item in items]
-
-with open('sim/center_locations.json', 'w') as f:
-    f.write(js.dumps(
-        locations,
-        indent=2
-    ))
+    items = [str(item) for item in list(temp['item_id'].values)]
+    orders_dict_x[str(order)] = items
 
 
 print(products_in_order_df.head())
@@ -51,9 +24,4 @@ print(products_in_order_df.head())
 batches = custom_alog.create_full_batches(orders_dict_x)
 
 
-'''
-# how many stops do we have per  (unique product / stops_number)
-# how many stops do we have per  (unique product / order_size)
-# 
-
-'''
+print(batches)
